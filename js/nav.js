4b04cc0db143af58a477bcee443488ab9091d325
Carlos Leon <cdleon15@yahoo.com>
2014-09-30 21:59:45 -0430

    periodicos nav

--- js/nav.js
+++ js/nav.js
@@ -18,3 +18,26 @@ $('.nav2-btn2').click(function(){
 		$(this).toggleClass('nav2-expanded').css('display','');
 	});
 });
+/**------------------
+** Periodicos mobile
+**------------------*/
+$('.p-mobile').click(function(){
+	$('.menu-periodicos-mobile').slideToggle(400, function(){
+		$(this).toggleClass('nav2-expanded').css('display','');
+	});
+});
+$('.pHeader-mobile').click(function(){
+	$(this).next().slideToggle(300, function(){
+		$(this).next().toggleClass('nav2-expanded').css('display','');
+	});
+});
+$(window).resize(function(){
+	if($(window).width() > threshold) {
+		if ($('.menu-periodicos-mobile').hasClass('nav2-expanded')) {
+			$('.menu-periodicos-mobile').removeClass('nav2-expanded');
+		};
+		if ($('.menu-periodicos-mobile').is(":visible")) {
+			$('.menu-periodicos-mobile').css('display','none');
+		};
+	}
+});
